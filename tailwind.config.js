552f1bf00d73ec56ba09ef6970eee3fb418213ec
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-06-07 13:59:38 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        "menu": "100px 1fr 100px",
+      },
+    },
+  },
+  plugins: [],
+}
+
